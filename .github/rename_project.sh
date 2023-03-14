89e2bc1a200805feba5394bd373c832c3862b81d
jakeywatson <jakeywatson@users.noreply.github.com>
2023-03-14 17:22:08 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jakeywatson"
+original_name="quantile_sensitivity"
+original_urlname="quantile-sensitivity"
+original_description="Awesome quantile_sensitivity created by jakeywatson"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv quantile_sensitivity $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from quantile_sensitivity.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "quantile_sensitivity"
